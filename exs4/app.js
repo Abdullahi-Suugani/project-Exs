08e10d4d5204085c7abbc7c52e207ae29a55509c
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-08-21 12:03:09 +0300

    Exs4

---
+++ exs4/app.js
@@ -0,0 +1,18 @@
+// Function Basics
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(5, 3));
+// console.log(add(10, 20));
+// console.log(add(7, 13));
+
+// Function Expression
+const add = function (a, b) {
+  return a + b;
+};
+
+console.log(add(5, 3));
+console.log(add(10, 20));
+console.log(add(7, 13));
